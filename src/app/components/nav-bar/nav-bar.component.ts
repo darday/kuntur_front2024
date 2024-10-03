c6a2f2fd7d6508ec7009d211deb08106465d90ba
darday <darday1980@gmail.com>
2024-10-03 10:30:06 -0500

    FT: FIX Navbar

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -4,18 +4,35 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [MatSlideToggleModule, MatToolbarModule, MatIconModule, MatButtonModule,CommonModule],
+  imports: [MatSlideToggleModule, MatToolbarModule, MatIconModule, MatButtonModule, CommonModule],
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent {
   menuOpen = false;
 
+  activeRoute = '/'; // Ruta inicial marcada por defecto
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    // Suscribirse a los eventos de navegación para detectar la ruta activa
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.activeRoute = event.urlAfterRedirects;
+      }
+    });
+  }
+
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
+  }
+
+  isRouteActive(route: string): boolean {
+    return this.activeRoute === route;
   }
 }
