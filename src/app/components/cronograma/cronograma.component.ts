a5a32622dc88e02a10f722772f608e7a497bd1b6
darday <darday1980@gmail.com>
2024-10-06 10:05:18 -0500

    Estructura de cronograma

--- src/app/components/cronograma/cronograma.component.ts
+++ src/app/components/cronograma/cronograma.component.ts
@@ -7,11 +7,22 @@ import { CommonModule } from '@angular/common';
 import { MatTabsModule } from '@angular/material/tabs';
 import {ChangeDetectionStrategy, signal} from '@angular/core';
 import {MatExpansionModule} from '@angular/material/expansion';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-cronograma',
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed,void', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
   standalone: true,
-  imports: [MatTabsModule, CommonModule,MatExpansionModule],
+  imports: [MatTabsModule, CommonModule,MatExpansionModule,MatButtonModule,MatIconModule,MatTableModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './cronograma.component.html',
   styleUrl: './cronograma.component.css'
@@ -19,6 +30,15 @@ import {MatExpansionModule} from '@angular/material/expansion';
 
 export class CronogramaComponent {
 
+
+  dataSource = ELEMENT_DATA;
+  columnsToDisplay = ['Peli', 'Director', 'Sinopsis','Enlace a Peli'];
+
+  columnsToDisplayWithExpand = [...this.columnsToDisplay, 'expand'];
+  expandedElement: PeriodicElement | null = null;
+
+  displayedColumns: string[] = ['position', 'name', 'symbol'];
+  dataSource2 = ELEMENT_DATA2;
   readonly panelOpenState = signal(false);
 
   days = [
@@ -91,4 +111,51 @@ export class CronogramaComponent {
   }
 
 
+
+
 }
+
+
+export interface PeriodicElement2 {
+  name: string;
+  position: any;
+  weight: any;
+  symbol: any;
+}
+
+const ELEMENT_DATA2: PeriodicElement2[] = [
+  {position: 'El extraño caso del hombre bala', name: 'Carlos Berenguer' , weight: 1.0079, symbol: 1},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+
+];
+
+export interface PeriodicElement {
+  Peli: string;
+  Director: string;
+  Sinópsis: string;
+  Enlace: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {
+    Peli: 'El extraño caso del hombre bala ',
+    Director: 'Carlos Berenguer',
+    Sinópsis: `Hydrogen is a chemical element with Sinópsis H and atomic number 1. With a standard atomic Director of 1.008, hydrogen is the lightest element on the periodic table.`,
+    Enlace: 'http://123',
+  },
+  {
+    Peli: 'El extraño caso del hombre bala ',
+    Director: 'Carlos Berenguer',
+    Sinópsis: `Hydrogen is a chemical element with Sinópsis H and atomic number 1. With a standard
+        atomic Director of 1.008, hydrogen is the lightest element on the periodic table.`,
+    Enlace: 'http://',
+  },
+  {
+    Peli: 'El extraño caso del hombre bala ',
+    Director: 'Carlos Berenguer',
+    Sinópsis: `Hydrogen is a chemical element with Sinópsis H and atomic number 1. With a standard
+        atomic Director of 1.008, hydrogen is the lightest element on the periodic table.`,
+    Enlace: 'http://',
+  },
+];
