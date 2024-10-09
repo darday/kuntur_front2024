413b516f4b5b58f0f3c0e86389d15cb63c86d825
darday <darday1980@gmail.com>
2024-10-09 06:04:20 -0500

    FT: Cronograma en Acerca del festival

--- src/app/pages/acerca-festival/acerca-festival.component.ts
+++ src/app/pages/acerca-festival/acerca-festival.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { CronogramaWithoutImgComponent } from '../../components/cronograma-without-img/cronograma-without-img.component';
+import { HttpClientModule, HttpClient } from '@angular/common/http'; // Importa HttpClientModule
+import { PatrocinadoresComponent } from '../../components/patrocinadores/patrocinadores.component';
+import { SuscriptionComponent } from '../../components/suscription/suscription.component';
+import { CronogramaComponent } from '../../components/cronograma/cronograma.component';
+
 
 @Component({
   selector: 'app-acerca-festival',
   standalone: true,
-  imports: [],
+  imports: [
+    CronogramaWithoutImgComponent,
+    PatrocinadoresComponent,
+    SuscriptionComponent,
+    CronogramaComponent
+  ],
   templateUrl: './acerca-festival.component.html',
   styleUrl: './acerca-festival.component.css'
 })
