e5c9f855519204edd544f8ac39e80e7092b18916
darday <darday1980@gmail.com>
2024-10-10 04:43:56 -0500

    FIX: Urls para producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     apiUrl: 'https://admin.festivalkunturnawi.com/api',
-    apiUrlStorage: 'https://admin.festivalkunturnawi.com/storage'
+    apiUrlStorage: 'https://admin.festivalkunturnawi.com/storage/app/public'
   };
   
